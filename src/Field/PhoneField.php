b4d25817ce01109fbe2ca9e384b66374801a1079
Jeremy Desvaux <jeremy.desvaux@wonderful.fr>
2019-07-10 11:47:23 +0200

    Better phone pattern

--- src/Field/PhoneField.php
+++ src/Field/PhoneField.php
@@ -7,7 +7,7 @@ class PhoneField extends InputField
     /** @inheritdoc */
     public function __construct($name, $value = null, array $displayRules = [], array $validationRules = [])
     {
-        $displayRules['inputAttributes']['pattern'] = "[\+]\d{2}[\(]\d{2}[\)]\d{4}[\-]\d{4}|^(?:0";
+        $displayRules['inputAttributes']['pattern'] = "^((\+\d{1,3}(-| )?\(?\d\)?(-| )?\d{1,5})|(\(?\d{2,6}\)?))(-| )?(\d{3,4})(-| )?(\d{4})(( x| ext)\d{1,5}){0,1}$";
         parent::__construct($name, $value, $displayRules, $validationRules);
         $this->type = 'tel';
     }
