3c4db358c1a8b5b5144868f2e9b1f151c17cf41d
Jeremy Desvaux <jeremy.desvaux@wonderful.fr>
2021-04-07 17:48:10 +0200

    Select Category Field : represent hierarchy with spacer

--- src/Field/CategoryField.php
+++ src/Field/CategoryField.php
@@ -4,15 +4,24 @@ namespace WonderWp\Component\Form\Field;
 
 class CategoryField extends AbstractCategoryField
 {
+    protected static $spacer = '- ';
 
     /**
      * @inheritDoc
      */
     public function doSetOptions()
     {
+        $genealogy = [];
         foreach ($this->categories as $category) {
             /** @var \WP_Term $category */
-            $this->selectOptions[$category->term_id] = $category->name;
+            if (!isset($genealogy[$category->parent])) {
+                $genealogy[$category->parent] = get_ancestors($category->term_id, 'category', 'taxonomy');
+            }
+        }
+        foreach ($this->categories as $category) {
+            /** @var \WP_Term $category */
+            $ancestors                               = isset($genealogy[$category->parent]) ? $genealogy[$category->parent] : [];
+            $this->selectOptions[$category->term_id] = str_repeat(static::$spacer, count($ancestors)) . $category->name;
         }
 
         $this->setOptions($this->selectOptions);
