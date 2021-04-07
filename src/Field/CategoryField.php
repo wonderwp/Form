9c8ac89a0c828c9afd2ee9d4d7ac8e9f61ef559c
Jeremy Desvaux <jeremy.desvaux@wonderful.fr>
2021-04-07 18:54:20 +0200

    Naming + phpdoc

--- src/Field/CategoryField.php
+++ src/Field/CategoryField.php
@@ -11,24 +11,33 @@ class CategoryField extends AbstractCategoryField
      */
     public function doSetOptions()
     {
-        $this->buildTree($this->categories, $this->parentCategory);
+        $this->makeHierarchical($this->categories, $this->parentCategory);
         $this->setOptions($this->selectOptions);
 
         return $this;
     }
 
-    protected function buildTree(array $elements, $parentId = 0, $level = 0)
+    /**
+     * Takes a flat WP_terms array and makes it hierarchical
+     *
+     * @param \WP_Term[] $categories
+     * @param int        $parentCatId the base of the tree
+     * @param int        $level       the depth level
+     *
+     * @return array
+     */
+    protected function makeHierarchical(array $categories, $parentCatId = 0, $level = 0)
     {
         $branch = [];
-        foreach ($elements as $element) {
-            if ($element->parent === $parentId) {
-                $this->selectOptions[$element->term_id] = str_repeat(static::$spacer, $level) . $element->name;
-                $children                               = $this->buildTree($elements, $element->term_id, $level + 1);
-                $element->level                         = $level;
+        foreach ($categories as $cat) {
+            if ($cat->parent === $parentCatId) {
+                $this->selectOptions[$cat->term_id] = str_repeat(static::$spacer, $level) . $cat->name;
+                $children                           = $this->makeHierarchical($categories, $cat->term_id, $level + 1);
+                $cat->level                         = $level;
                 if ($children) {
-                    $element->children = $children;
+                    $cat->children = $children;
                 }
-                $branch[] = $element;
+                $branch[] = $cat;
             }
         }
 
