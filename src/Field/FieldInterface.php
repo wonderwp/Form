2eba6077b5dd5b090454d890b682470f7d05d827
Jeremy Desvaux <jeremy.desvaux@wonderful.fr>
2021-12-07 11:35:46 +0100

    doc(form/interfaces) maj phpdoc

--- src/Field/FieldInterface.php
+++ src/Field/FieldInterface.php
@@ -7,25 +7,13 @@ use Respect\Validation\Validator;
 interface FieldInterface
 {
     /**
-     * @param array $passedRules
-     *
-     * @return static
-     */
-    public function computeDisplayRules(array $passedRules);
-
-    /**
-     * @param Validator[] $passedRules
-     *
-     * @return static
-     */
-    public function computeValidationRules(array $passedRules);
-
-    /**
+     * Name getter
      * @return string
      */
     public function getName();
 
     /**
+     * Name setter
      * @param string $name
      *
      * @return static
@@ -33,11 +21,13 @@ interface FieldInterface
     public function setName($name);
 
     /**
+     * Tag getter
      * @return string
      */
     public function getTag();
 
     /**
+     * Tag setter
      * @param string $tag
      *
      * @return static
@@ -45,11 +35,13 @@ interface FieldInterface
     public function setTag($tag);
 
     /**
+     * Type getter
      * @return string
      */
     public function getType();
 
     /**
+     * Type setter
      * @param string $type
      *
      * @return static
@@ -57,11 +49,13 @@ interface FieldInterface
     public function setType($type);
 
     /**
+     * Value getter
      * @return mixed
      */
     public function getValue();
 
     /**
+     * Value setter
      * @param mixed $value
      *
      * @return static
@@ -69,11 +63,13 @@ interface FieldInterface
     public function setValue($value);
 
     /**
+     * Display rules getter
      * @return array
      */
     public function getDisplayRules();
 
     /**
+     * Display rules setter
      * @param array $displayRules
      *
      * @return static
@@ -81,11 +77,13 @@ interface FieldInterface
     public function setDisplayRules(array $displayRules);
 
     /**
+     * Validation rules getter
      * @return Validator[]
      */
     public function getValidationRules();
 
     /**
+     * Validation rules setter
      * @param Validator[] $validationRules
      *
      * @return static
@@ -93,11 +91,13 @@ interface FieldInterface
     public function setValidationRules(array $validationRules);
 
     /**
+     * Errors getter
      * @return array
      */
     public function getErrors();
 
     /**
+     * Errors setter
      * @param array $errors
      *
      * @return static
@@ -105,11 +105,13 @@ interface FieldInterface
     public function setErrors($errors);
 
     /**
+     * Rendered getter
      * @return bool
      */
     public function isRendered();
 
     /**
+     * Rendered setter
      * @param bool $rendered
      *
      * @return static
@@ -117,6 +119,33 @@ interface FieldInterface
     public function setRendered($rendered);
 
     /**
+     * Should apply $passedRules to the field display rules
+     * Can also be used to specify a set of default display rules you can merge $passedRules, then assign to the field display rules
+     * @param array $passedRules
+     *
+     * @see http://wonderwp.net/Framewok_components/Forms/The_Form_Fields.html#page_Display_Rules   Display rules doc
+     * @see https://github.com/wonderwp/Form/blob/develop/src/Field/AbstractField.php#L155 Implementation example
+     *
+     * @return static
+     */
+    public function computeDisplayRules(array $passedRules);
+
+    /**
+     * Should apply $passedRules to the field validation rules
+     * Can also be used to specify a set of default validation rules you can merge $passedRules, then assign to the field validation rules
+     *
+     * @see http://wonderwp.net/Framewok_components/Forms/The_Form_Fields.html#page_Validation_Rules Validation rules doc
+     * @see https://github.com/wonderwp/Form/blob/develop/src/Field/AbstractField.php#L175 Implementation example
+     *
+     * @param Validator[] $passedRules
+     *
+     * @return static
+     */
+    public function computeValidationRules(array $passedRules);
+
+    /**
+     * Turn the FieldInterface instance into an array
+     * Useful to quickly consume the field instance from a different context, as a json object for an API for example.
      * @return array
      */
     public function toArray();
--- src/FormInterface.php
+++ src/FormInterface.php
@@ -4,14 +4,19 @@ namespace WonderWp\Component\Form;
 
 use WonderWp\Component\Form\Field\FieldInterface;
 
+/**
+ * @see https://github.com/wonderwp/Form/blob/develop/src/Form.php for an implementation example
+ */
 interface FormInterface
 {
     /**
+     * Name getter
      * @return string
      */
     public function getName();
 
     /**
+     * Name setter
      * @param string $name
      *
      * @return static
@@ -19,11 +24,19 @@ interface FormInterface
     public function setName($name);
 
     /**
+     * Fields getter
      * @return FieldInterface[]
      */
     public function getFields();
 
     /**
+     * Fields setter
+     * @param array FieldInterface[] $fields
+     */
+    public function setFields(array $fields);
+
+    /**
+     * Retrieve a specific form field among form fields, given its name
      * @param string $name
      *
      * @return FieldInterface
@@ -31,14 +44,16 @@ interface FormInterface
     public function getField($name);
 
     /**
-     * @param FieldInterface $field
-     * @param string         $groupName
+     * Add a new field to the form
+     * @param FieldInterface|null $field
+     * @param string $groupName
      *
      * @return static
      */
     public function addField(FieldInterface $field = null, $groupName = '');
 
     /**
+     * Remove several fields from the form, given an array of field names to remove
      * @param array $fieldNames
      *
      * @return static
@@ -46,33 +61,25 @@ interface FormInterface
     public function removeFields(array $fieldNames);
 
     /**
-     * @param array FieldInterface[] $fields
-     */
-    public function setFields(array $fields);
-
-    /**
+     * Errors getter
      * @return array
      */
     public function getErrors();
 
     /**
+     * Errors setter
      * @param array $errors
      */
     public function setErrors($errors);
 
     /**
+     * Groups getter
      * @return FormGroup[]
      */
     public function getGroups();
 
     /**
-     * @param string $name
-     *
-     * @return FormGroup
-     */
-    public function getGroup($name);
-
-    /**
+     * Groups setter
      * @param FormGroup[] $groups
      *
      * @return static
@@ -80,6 +87,15 @@ interface FormInterface
     public function setGroups(array $groups);
 
     /**
+     * Get a particular group, from groups, given its name
+     * @param string $name
+     *
+     * @return FormGroup
+     */
+    public function getGroup($name);
+
+    /**
+     * Add a new singular group
      * @param FormGroup $group
      *
      * @return static
@@ -87,6 +103,7 @@ interface FormInterface
     public function addGroup(FormGroup $group);
 
     /**
+     * Remove a singular group
      * @param string $name
      *
      * @return static
@@ -94,6 +111,7 @@ interface FormInterface
     public function removeGroup($name);
 
     /**
+     * Take an array of data, ([fieldName=>fieldValue]), and set field values to corresponding fields
      * @param array $data
      *
      * @return static
@@ -101,22 +119,27 @@ interface FormInterface
     public function fill(array $data);
 
     /**
+     * Shortcut to get a correspondig FormView object
      * @return FormView
      */
     public function getView();
 
-    /** Render Form View
+    /**
+     * Render Form View
      * @param array $opts ,opt to pass to the view
      * @return string
      */
     public function renderView(array $opts = []);
 
     /**
+     * Get each field value in an array
      * @return array
      */
     public function getValues();
 
     /**
+     * Turn the FormInterface object into an array
+     * Useful to quickly consume the form instance from a different context, as a json object for an API for example.
      * @return array
      */
     public function toArray();
